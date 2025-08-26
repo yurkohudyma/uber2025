4dfaeab9f68434d33d4cad4fab58025a9436d5e3
yurkohudyma <hudyma@gmail.com>
2025-08-26 13:29:46 +0300

    added rating-service

---
+++ rating-service/src/main/java/ua/hudyma/ratingservice/RatingServiceApplication.java
@@ -0,0 +1,14 @@
+package ua.hudyma.ratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RatingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RatingServiceApplication.class, args);
+    }
+}
+
