8ebae8bb5d5d308b1e3fb004ad166794838434c7
yurkohudyma <hudyma@gmail.com>
2025-08-27 12:43:51 +0300

    added payment-service

---
+++ payment-service/src/main/java/ua/hudyma/paymentservice/config/KafkaConfig.java
@@ -0,0 +1,9 @@
+package ua.hudyma.paymentservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+}
---
+++ payment-service/src/main/java/ua/hudyma/paymentservice/config/WebClientConfig.java
@@ -0,0 +1,30 @@
+package ua.hudyma.paymentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient geoServiceWebClient(Builder builder) {
+        return builder.baseUrl("http://localhost:9094").build();
+    }
+
+    @Bean
+    public WebClient userServiceWebClient(Builder builder) {
+        return builder
+                .baseUrl("http://localhost:9091").build();
+    }
+
+    @Bean
+    public WebClient rideServiceWebClient(Builder builder) {
+        return builder
+                .baseUrl("http://localhost:9097").build();
+    }
+}
+
+
+
